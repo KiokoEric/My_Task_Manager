c570d9f679660434ed7d81ab44312b9100b382aa
KiokoEric <kiokoerick040@gmail.com>
2024-12-24 17:54:01 +0300

    Update to the Task Master Web Application.

--- task_master/src/main.jsx
+++ task_master/src/main.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import App from './App.jsx';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import AppContextProvider from './Components/Context/AppContext';
+import AppContextProvider from './Components/Context/AppContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AppContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AppContextProvider>
+  <BrowserRouter>
+    <AppContextProvider>
+        <App />
+    </AppContextProvider>
+  </BrowserRouter>
 )
